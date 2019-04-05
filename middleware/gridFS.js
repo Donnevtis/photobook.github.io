09f27ee99fba3ee25bdd81a982eac81048c93ee1
Donald Nevtis <nevtees@gmail.com>
2019-04-06 00:23:35 +0300

    async upload

--- middleware/gridFS.js
+++ middleware/gridFS.js
@@ -8,6 +8,7 @@ const streamifier = require('streamifier');
 //gridFS middleware
 class GridFS {
     constructor() {
+        console.log('check');
         this.client = new mongodb.MongoClient(config.uri, { useNewUrlParser: true });
         this.FILES_COLL = 'photos.files';
         this.client.connect((error) => {
@@ -17,25 +18,24 @@ class GridFS {
             });
             this.collection = this.db.collection(this.FILES_COLL)
         })
-
     }
 
-    sendFile(name, file, data) {
+    async sendFile(name, file, data) {
+        this.t = Date.now();
         this.uploadStream = this.bucket.openUploadStream(name, {
             metadata: data
         })
         this.id = this.uploadStream.id;
-
-
         return new Promise(resolve => {
             streamifier.createReadStream(file).
             pipe(this.uploadStream).
             on('error', function(error) {
                 assert.ifError(error);
             }).
-            on('finish', function() {
-                console.log('done!');
+            on('finish', () => {
+                console.log('done!', (Date.now() - this.t));
                 resolve(this.id)
+
             });
         });
     }
@@ -44,6 +44,10 @@ class GridFS {
         const result = this.db.collection(this.FILES_COLL).find({
             'metadata.album': album._id,
             'metadata.min': 'true'
+        }, {
+            sort: [
+                ['uploadDate', -1]
+            ]
         })
         return result;
     }
@@ -57,5 +61,39 @@ class GridFS {
 
 }
 
+async function send(name, file, data) {
+    return new Promise(resolve => {
+        console.log('checks');
+        const client = new mongodb.MongoClient(config.uri, { useNewUrlParser: true });
+        const FILES_COLL = 'photos.files';
+        client.connect((error) => {
+            const db = client.db();
+            const bucket = new mongodb.GridFSBucket(db, {
+                bucketName: 'photos'
+            });
+            const collection = db.collection(FILES_COLL)
+            let t = Date.now();
+            const uploadStream = bucket.openUploadStream(name, {
+                metadata: data
+            })
+            const id = uploadStream.id;
+
+            streamifier.createReadStream(file).
+            pipe(uploadStream).
+            on('error', function(error) {
+                assert.ifError(error);
+            }).
+            on('finish', () => {
+                console.log('done!', (Date.now() - t));
+                resolve(id)
+            });
+        });
+        client.close()
+    });
+}
+
+
+
 
 module.exports = new GridFS;
+module.exports.send = send;
